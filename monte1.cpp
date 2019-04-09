a6ac3099f1c894b0f85adab808836e10d4bb9c4b
Nathaliacr <46941173+Nathaliacr@users.noreply.github.com>
2019-04-09 14:36:47 -0500

    Update monte1.cpp

--- monte1.cpp
+++ monte1.cpp
@@ -1,33 +1,65 @@
 #include <fstream>
 #include <iostream>
-using namespace std;
- 
-void leer(string filename);
+#include <string.h>
+using namespace std; 
 
-int main () {
-  string filename1;
-  string filename2;
-  filename1 = "valores_x.txt";
-  filename2 = "valores_y.txt";
-  leer(filename1);
-  leer(filename2);
+float * read_file(string filename, int *n_points);
+float add_values(float *x, int n_points);
 
+int main(){
+  float *x=NULL;
+  float *y=NULL;
+  int n_x=0;
+  int n_y=0;
+
+  x = read_file("valores_x.txt", &n_x);
+  y = read_file("valores_y.txt", &n_y);
+  
+  cout << add_values(x, n_x) << " " << add_values(y, n_y) << endl;
+  
   return 0;
 }
 
-void leer(string filename){
+float * read_file(string filename, int *n_points){
+  int n_lines=0;
   ifstream infile; 
   string line;
+  int i;
+  float *array;
 
+  /*cuenta lineas*/
   infile.open(filename); 
-  
-  cout << "Leyendo de " << filename << endl; 
   getline(infile, line);
   while(infile){
-    cout << line << endl;
+    n_lines++;
     getline(infile, line);
   }
-
   infile.close();
+  *n_points = n_lines;
+
+  /*reserva la memoria necesaria*/
+  array = new float[n_lines];
+
+  /*carga los valores*/
+  i=0;
+  infile.open(filename); 
+  getline(infile, line);  
+  while(infile){
+    array[i] = atof(line.c_str());
+    i++;
+    getline(infile, line);
+  }
+  infile.close();
+
+  return array;
 }
 
+float add_values(float *x, int n_points){
+  float a=0;
+  int i;
+  
+  for(i=0;i<n_points;i++){
+    a += x[i];
+  }
+  return a;
+}
